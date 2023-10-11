4a32edcd4bb567df45f7a951dfa1b4158326c474
Arjun Maharanan <arjunmaharanan@Arjuns-MacBook-Air.local>
2023-10-11 15:56:02 +0530

    this code is working or not

--- java_DSA/src/pratice_section/Accenture/test2.java
+++ java_DSA/src/pratice_section/Accenture/test2.java
@@ -1,2 +1,22 @@
-package pratice_section.Accenture;public class test2 {
+package pratice_section.Accenture;
+import java.util.*;
+public class test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the num1 ");
+        int num1 = sc.nextInt();
+        System.out.println("enter the num2 ");
+        int num2 = sc.nextInt();
+        int res = sol(num1,num2);
+        System.out.println(res);
+    }
+    static int sol(int num1, int num2) {
+        int sum = 0;
+        for (int i = 1; i <= num1; i++) {
+            if (i % num2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
